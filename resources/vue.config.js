a63a4268eae186967f6dcd7bce0fed9fc75d7001
Alfian Arga Budiyanto <alvianarga1@gmail.com>
2020-04-14 13:52:13 +0700

    commit pertama

---
+++ resources/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //publicPath: 'dist/',
+    publicPath: process.env.NODE_ENV === 'production' ? 'dist/' : './',
+    outputDir: '../dist',
+    indexPath: '../index.html',
+    // devServer: {
+    //     public: 'localhost:8080',
+    //     host: 'localhost',
+    //     port: 8080
+    // }
+}
