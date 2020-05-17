a0f41466a719f79b00b707b4181def8aa53bafb3
Alfian Arga Budiyanto <alvianarga1@gmail.com>
2020-05-17 10:44:30 +0700

    buat file env dan penerapan BASE_URL dari file env

--- resources/src/components/formNotes.vue
+++ resources/src/components/formNotes.vue
@@ -39,7 +39,7 @@
                 params.append('title', this.title);
                 params.append('description', this.description);
 
-                axios.post('http://notes.wegodev/api/notes', params).then(response => {
+                axios.post(process.env.VUE_APP_BASE_URL+'/api/notes', params).then(response => {
                     let data = {
                     id : response.data.id,
                     title : this.title,
@@ -54,7 +54,7 @@
                 params.append('title', this.title);
                 params.append('description', this.description);
 
-                axios.put('http://notes.wegodev/api/notes/'+this.id, params).then(response => {
+                axios.put(process.env.VUE_APP_BASE_URL+'/api/notes/'+this.id, params).then(response => {
                     let data = {
                     id : response.data.id,
                     title : this.title,
@@ -67,7 +67,7 @@
                 let params = new URLSearchParams();
                 params.append('id', this.id);
 
-                axios.delete('http://notes.wegodev/api/notes/'+this.id, params).then(response => {
+                axios.delete(process.env.VUE_APP_BASE_URL+'/api/notes/'+this.id, params).then(response => {
                     let data = {
                     id : response.data.id
                 }
